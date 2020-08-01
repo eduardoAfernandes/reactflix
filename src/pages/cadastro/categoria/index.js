16fb19097fa41245cc1495f92824d29f6711181a
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-08-01 02:11:24 -0300

    Final Aula 4 - Ajax e Mais

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import FormField from '../../../components/FormField';
+import FormField from '../../../components/FormField/index';
 import Button from '../../../components/Button';
 
 function CadastroCategoria() {
